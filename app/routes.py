f4e6d184cf7a809db17b94017b7ece56922734ad
Ubuntu <ubuntu@ip-172-31-7-211.ap-southeast-2.compute.internal>
2020-05-22 13:31:15 +0000

    login page added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
-from flask import Flask, render_template, request
+from flask import Flask, render_template, flash, request, redirect, url_for
+from app.forms import LoginForm
 
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
 class Config(object):
-    # Class for adding environment variables
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
