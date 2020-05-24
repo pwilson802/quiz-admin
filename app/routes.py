603a44bd8ffead6067ed8cec6062a1845964994a
Ubuntu <ubuntu@ip-172-31-7-211.ap-southeast-2.compute.internal>
2020-05-24 12:19:44 +0000

    user logins working

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,36 @@
 from app import app
 from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.urls import url_parse
 from app.forms import LoginForm
+from flask_login import login_required
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
-@app.route('/login', methods=["POST", "GET"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route("/", methods=["POST", "GET"])
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/', methods=["POST", "GET"])
+@app.route('/index', methods=["POST", "GET"])
+@login_required
 def index():
     return render_template('index.html')
