aa28234c8f4d8052a8da9f4c36782f25839cab0f
Ubuntu <ubuntu@ip-172-31-7-211.ap-southeast-2.compute.internal>
2020-05-26 12:23:05 +0000

    get_question mod.py working

--- app/mod.py
+++ app/mod.py
@@ -6,7 +6,12 @@ cat_map = {
 
 }
 
+def check_question_db():
+    # TODO - CHeck if a question is already added to dynamodb
+
 def get_question(category, difficulty="medium"):
+    # Get a single question from the trivia API
+    # Returns a dictionary with category, type, difficulty, question, correct_answer, incorrect_answer
     url = "https://opentdb.com/api.php"
     params = {
         'amount': '10',
@@ -16,4 +21,6 @@ def get_question(category, difficulty="medium"):
     }
     all_questions = json.loads(requests.get(url, params=params).text)['results']
     return_question = all_questions[0]
+    return_question['category'] = category
     return return_question
+
--- app/static/script/main.js
+++ app/static/script/main.js
@@ -4,3 +4,9 @@ let apiSection = document.querySelector('.get-api')
 showButton.addEventListener('click', function(){
     apiSection.classList.remove("hide")
 })
+
+console.log(questionData)
+
+const questionField = document.querySelector('.question')
+const correctAnswer = document.querySelector('.correct_answer')
+const 
