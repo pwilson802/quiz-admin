236999065178afe31dc449efd7c9b1c65a12b4da
Ubuntu <pwilson802@gmail.com>
2020-06-18 12:24:11 +0000

    added add page

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ from app.mod import get_question_api, add_question, get_question_csv
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('qq_add'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -20,20 +20,26 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('qq_add')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('qq_add'))
 
 
-@app.route('/', methods=["POST", "GET"])
-@app.route('/index', methods=["POST", "GET"])
+@app.route('/', methods=["GET"])
+@app.route('/index', methods=["GET"])
 @login_required
 def index():
+    return render_template('index.html')
+
+
+@app.route('/qq_add', methods=["POST", "GET"])
+@login_required
+def qq_add():
     if request.method == 'POST':
         request_type = request.values.get('action')
         if request_type == 'get_question_api':
@@ -41,11 +47,11 @@ def index():
             difficulty = request.values.get("difficulty")
             data = get_question_api(category, difficulty)
             data['source'] = 'api'
-            return render_template('index.html', data=data)
+            return render_template('qq_add.html', data=data)
         if request_type == 'get_question_csv':
             data = get_question_csv()
             data['source'] = 'csv'
-            return render_template('index.html', data=data)
+            return render_template('qq_add.html', data=data)
         if request_type == 'post_question':
             print(request.values.to_dict())
             question_dict = request.values.to_dict()
@@ -54,6 +60,6 @@ def index():
                 # This looks really bad and should be fixed
                 flash('Question is already in the database')
             #post the question to dynamodb and give a response
-            return render_template('index.html')
+            return render_template('qq_add.html')
     else:
-        return render_template('index.html')
+        return render_template('qq_add.html')
